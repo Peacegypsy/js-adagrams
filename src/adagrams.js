28e7aa1faa680af6cca10cbab090a6a2db82055c
Xtina <2006peacegypsy@gmail.com>
2018-11-15 10:00:31 -0800

    figuring out what works in removing letters from pool

--- src/adagrams.js
+++ src/adagrams.js
@@ -62,20 +62,71 @@ const Adagrams = {
       "N",
       "O",
       "O",
-      "O"
+      "O",
+      "O",
+      "O",
+      "O",
+      "O",
+      "O",
+      "P",
+      "P",
+      "Q",
+      "R",
+      "R",
+      "R",
+      "R",
+      "R",
+      "R",
+      "S",
+      "S",
+      "S",
+      "S",
+      "T",
+      "T",
+      "T",
+      "T",
+      "T",
+      "T",
+      "U",
+      "U",
+      "U",
+      "U",
+      "V",
+      "V",
+      "W",
+      "W",
+      "X",
+      "Y",
+      "Y",
+      "Z"
     ];
     // Implement this method for wave 1
     let hand = [];
     let tilesDrawn = 0;
-    let rand = letterPool[Math.floor(Math.random() * letterPool.length)];
+    // let rand = letterPool[Math.floor(Math.random() * letterPool.length)];
     while (tilesDrawn < 10) {
-      for (rand in letterPool) {
-        hand.push(rand);
-        tilesDrawn++;
-      }
+      // for (let i = 0; i < 10; i++) {
+      let rand = letterPool[Math.floor(Math.random() * letterPool.length)];
+      // if (rand > 0);
+      // rand -= 1;
+      hand.push(rand);
+      tilesDrawn++;
     }
-    hand = hand.join();
+
+    return hand;
   }
+
+  // usesAvailableLetters(input, letterInHand){
+  //   newLetters = letterInHand.map(letter)
+  //   input.split("").forEach(letter);
+  //   if (!newLetters).includes(letter)
+  //   return false
+  //   else if newLetters.includes(letter);
+  //   index = newLetters.findIndex(letter);
+  //   newLetters.deleteAt(index)
+  // },
+  // return true
 };
+
 // Do not remove this line or your tests will break!
 export default Adagrams;
