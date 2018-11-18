2ed3f5507380217bf1f81f2ee556e39fcbedf74b
Xtina <2006peacegypsy@gmail.com>
2018-11-18 10:39:49 -0800

    working on scoring words

--- src/adagrams.js
+++ src/adagrams.js
@@ -130,8 +130,7 @@ const Adagrams = {
   usesAvailableLetters(input, hand) {
     const availableLetters = input.split("");
     const handCopy = hand.slice(0);
-    console.log(hand);
-    console.log(input);
+
     return availableLetters.every(letter => {
       const index = handCopy.indexOf(letter);
       if (index === -1) return false;
@@ -141,20 +140,36 @@ const Adagrams = {
     });
   }
 };
+ scoreWord(word)
+  wordScore = 0
+  letters = word.split("").toUpperCase;
+  for(letter of letters){
 
-// if (!availableLetters.includes(input.split())) { //make this a forEach ??
-//   console.log("choose a different word");
-//   console.log("We hit 2");
-//   return false;
-// } else {
-// console.log("We hit 1");
-// if (availableLetters.includes(input.split().toUpperCase()));
-// {
-//   let index = availableLetters.findIndex(input);
-//   {
-//     availableLetters.deleteAt(index);
-//     console.log("Good play");
-//   }
-// }
+  switch (letter) {
+    case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
+    wordScore += 1
+    break;
+    case "D", "G":
+    wordScore += 2
+    break;
+    case "B", "C", "M", "P":
+    wordScore += 3
+    break;
+    case "F", "H", "V", "W", "Y":
+    wordScore += 4
+    break;
+    case "K":
+    wordScore += 5
+    case "J", "X":
+    wordScore += 8
+    break;
+    case "Q", "Z":
+    wordScore += 10
+    break;
+    default:
+    console.log("That is not a letter.");
+  // word_score += 8 if(7..10).includes?(word.length);
+  return word_score;
+}}}
 
 export default Adagrams;
